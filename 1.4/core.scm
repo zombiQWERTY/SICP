293d971ed4cb1f22a0b3e834197849d7978773d3
zombiQWERTY <zombiqwerty@yandex.ru>
2017-05-05 03:01:02 +0300

    Task 1.4

--- 1.4/core.scm
+++ 1.4/core.scm
@@ -1,8 +1,1 @@
 (load "common.scm")
-
-(define (get-sum a b c)
-    (cond ((and (< a b) (< a c)) (+ (square b) (square c)))
-            ((and (< b a) (< b c)) (+ (square a) (square c)))
-            (else (+ (square a) (square b)))))
-
-(get-sum 4 9 100)
